76021a25eea2fe3b31682b4c525f362757935640
fer <ferp@protonmail.ch>
2021-12-06 11:58:58 +0100

    project scaffolding

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function newContainer(className) {
+  const element = document.createElement('div')
+  element.className = className
+  return element
+}
+
+const run = async () => {
+ 
+  return
+}
+
+document.body.appendChild(newContainer('line-container'))
+run()
