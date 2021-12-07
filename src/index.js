5b894df75798509a6a15b1b5e35079cedba0acd0
fer <ferp@protonmail.ch>
2021-12-07 13:14:32 +0100

    added responsive legend

--- src/index.js
+++ src/index.js
@@ -19,24 +19,59 @@ const run = async () => {
       })
     ) 
   )
-  console.log(lineData[0])
+  console.log(lineData)
 
   // create container
   const domContainer = newContainer('line-container')
   await document.body.appendChild(domContainer)
   const containerWidth = domContainer.getBoundingClientRect().width;
 
-  // insert chart
+  const domLegendContainer = newContainer('legend-container')
+  await document.body.appendChild(domLegendContainer)
+
+  // insert chart with tooltip and legend
   const lineChart = britecharts.line()
+  const chartTooltip = britecharts.tooltip()
+  const chartLegend = britecharts.legend()
+
   lineChart
     .margin({ bottom: 50 })
     .height(400)
     .width(containerWidth)
+    .on('customMouseOver', chartTooltip.show)
+    .on('customMouseMove', chartTooltip.update)
+    .on('customMouseOut', chartTooltip.hide)
 
   const container = d3.select('.line-container')
   container.datum({data: [...lineData[0], ...lineData[1]]}).call(lineChart);
   
-  // make it responsive: adapt to width changes
+  // tooltip
+  const tooltipContainer = d3.select('.line-container .metadata-group .hover-marker')
+  tooltipContainer.call(chartTooltip)
+
+  // legend
+  const legendContainer = d3.select('.legend-container')
+  const legendData = [...lineData[0], ...lineData[1]].reduce(
+    (ac, item) => {
+      let found = ac.find(elem => elem.id === item.name)
+      if (found) { return ac }
+      return [
+        {
+          id: item.name,
+          name: item.topicName
+        }, 
+        ...ac
+      ]
+    },
+    []
+  )
+  chartLegend
+    .width(containerWidth)
+    .height(60)
+    .isHorizontal(true)
+  legendContainer.datum(legendData).call(chartLegend)
+  
+  // make the chart responsive: adapt to width changes
   const redrawChart = () => {
     const newContainerWidth = container.node() 
       ? container.node().getBoundingClientRect().width
@@ -44,7 +79,10 @@ const run = async () => {
 
     lineChart.width(newContainerWidth)
     container.call(lineChart)
+    chartLegend.width(newContainerWidth)
+    legendContainer.call(chartLegend)
   } 
+
 
   window.addEventListener('resize', () => window.requestAnimationFrame(redrawChart))
 
